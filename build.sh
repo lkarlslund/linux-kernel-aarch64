2c9b81c9837f05c1395b5ea0f6bbe7310740e844
Lars Karlslund <lars@karlslund.dk>
2025-02-25 13:35:31 +0100

    Proper unsetting

--- build.sh
+++ build.sh
@@ -5,9 +5,10 @@ cd kernel
 echo Oldconfig ...
 
 make oldconfig
+scripts/config --set-str SYSTEM_TRUSTED_KEYS ""
 
 # No signing of debs
-CONFIG_SYSTEM_TRUSTED_KEYS = ""
+# CONFIG_SYSTEM_TRUSTED_KEYS = ""
 
 echo Building debs ...
 nice make -j`nproc` bindeb-pkg
